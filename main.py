8bd5baaa0eac749f9fb4c2c5db113be6d9e10746
Cephra <soulzterx@googlemail.com>
2014-05-22 17:07:02 +0200

    ...

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import HL2
+import math
 
-cfg = HL2.CfgBuilder("testscript")
+cfg = HL2.CfgBuilder("manysparks")
 
-ent = HL2.Entity(cfg,
-"prop_dynamic_override",
-"targetnamegoeshere",
-"models/props_junk/wood_crate001a.mdl")
-ent.create()
+for x in range(0,36,1):
+    ent = HL2.Entity(cfg,
+            "env_spark",
+            "spark{0}".format(x))
+    ent.create()
+
+    rad = 100
+    rads = math.radians(x*10)
+    coords = "{0} {1} 0".format(
+                math.cos(rads)*rad,
+                math.sin(rads)*rad
+            )
+    ent.setKeyvalue("origin", coords)
+
+    ent.fireInput("startspark")
 
 cfg.build()
