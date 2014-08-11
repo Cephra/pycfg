e68bd904dff6c70168baf83d3d2354d01cef2a98
Christian Frantzen <soulzterx@googlemail.com>
2014-08-11 18:41:36 +0200

    Changed file creation algorithm and now introduced cfg.newFile()

--- example.py
+++ example.py
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 import cfgents
 import cfgfile
 import cfgmath
 
 # Create a script by the name "manysparks".
-=======
-import cfgfile
-import cfgents
-import cfgmath
-
-# Create a script by the name "manysparks". 
->>>>>>> fd1213a1d373efe8d89d4d28b220fcc981e21ac9
 cfg = cfgfile.CfgBuilder("manysparks")
 
 # Define the Origin and the Point we want to rotate.
@@ -19,12 +11,9 @@ v = cfgmath.Vec(100,0,0)
 
 # Rotate by 10 degrees every 36 times => 360 degrees.
 for x in range(0,36,1):
-<<<<<<< HEAD
     if ((x%4)==0):
         cfg.newFile();
 
-=======
->>>>>>> fd1213a1d373efe8d89d4d28b220fcc981e21ac9
     ent = cfgents.Entity(cfg,
             "env_spark",
             "spark{0}".format(x))
